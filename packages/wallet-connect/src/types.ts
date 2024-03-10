2eb3410155c76e5db063db2b435c96d1f132099c
Daria Mikhailova <daria@parity.io>
2024-03-10 13:00:56 +1300

    adding wallet connect modal optional configuration - options

--- packages/wallet-connect/src/types.ts
+++ packages/wallet-connect/src/types.ts
@@ -4,11 +4,31 @@ export type WcAccount = `${string}:${string}:${string}`;
 
 export type PolkadotNamespaceChainId = `polkadot:${string}`;
 
+export interface WalletOptionsType {
+  id: string;
+  name: string;
+  links: {
+    native: string;
+    universal?: string;
+  };
+}
+
 export interface WalletConnectConfiguration extends SignClientTypes.Options {
   // ToDo: Remove ```projectId``` when the following issue is resolved:
   // https://github.com/WalletConnect/walletconnect-monorepo/pull/3435
   projectId: string;
   chainIds?: PolkadotNamespaceChainId[];
+  options?: {
+    mobileWallets?: WalletOptionsType[];
+    desktopWallets?: WalletOptionsType[];
+    walletImages?: Record<string, string>;
+    enableAuthMode?: boolean;
+    enableExplorer?: boolean;
+    explorerRecommendedWalletIds?: string[] | 'NONE';
+    explorerExcludedWalletIds?: string[] | 'ALL';
+    termsOfServiceUrl?: string;
+    privacyPolicyUrl?: string;
+  };
   optionalChainIds?: PolkadotNamespaceChainId[];
   onSessionDelete?: () => void;
 }
